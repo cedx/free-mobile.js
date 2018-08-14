b30f5121bb68d58edc1fa42a6143db8c1684d827
Cédric Belin <cedric@belin.io>
2018-08-15 00:46:34 +0200

    Using decorated classes for the unit tests

--- test/client_test.ts
+++ test/client_test.ts
@@ -1,64 +1,62 @@
+/* tslint:disable: no-unused-expression */
 import {expect} from 'chai';
-const {Client, ClientError} from '../lib';
+import {suite, test, timeout} from 'mocha-typescript';
+import {Client, ClientError} from '../src';
 
 /**
- * @test {Client}
+ * Tests the features of the `Client` class.
  */
-describe('Client', function() {
-  this.timeout(15000);
+@suite(timeout(15000))
+class ClientTest {
 
   /**
    * @test {Client#constructor}
    */
-  describe('constructor', () => {
-    it('should throw an error if the credentials are invalid', async () => {
-      try {
-        await new Client('', '').sendMessage('Hello World!');
-        expect.fail('Error not thrown');
-      }
+  @test public async testConstructor(): Promise<void> {
+    // It should throw an error if the credentials are invalid.
+    try {
+      await new Client('', '').sendMessage('Hello World!');
+      expect.fail('Error not thrown');
+    }
 
-      catch (err) {
-        expect(err).to.be.an.instanceof(TypeError);
-      }
-    });
-  });
+    catch (err) {
+      expect(err).to.be.an.instanceof(TypeError);
+    }
+  }
 
   /**
    * @test {Client#sendMessage}
    */
-  describe('#sendMessage()', () => {
-    it('should not send invalid messages', async () => {
-      try {
-        await new Client('anonymous', 'secret').sendMessage('');
-        expect.fail('Error not thrown');
-      }
+  @test public async testSendMessage(): Promise<void> {
+    // It should not send invalid messages.
+    try {
+      await new Client('anonymous', 'secret').sendMessage('');
+      expect.fail('Error not thrown');
+    }
 
-      catch (err) {
-        expect(err).to.be.an.instanceof(TypeError);
-      }
-    });
+    catch (err) {
+      expect(err).to.be.an.instanceof(TypeError);
+    }
 
-    it('should throw a `ClientError` if a network error occurred', async () => {
-      try {
-        await new Client('anonymous', 'secret', 'http://localhost').sendMessage('Bonjour Cédric !');
-        expect.fail('Error not thrown');
-      }
+    // It should throw a `ClientError` if a network error occurred.
+    try {
+      await new Client('anonymous', 'secret', 'http://localhost').sendMessage('Bonjour Cédric !');
+      expect.fail('Error not thrown');
+    }
 
-      catch (err) {
-        expect(err).to.be.an.instanceof(ClientError);
-      }
-    });
+    catch (err) {
+      expect(err).to.be.an.instanceof(ClientError);
+    }
 
+    // It should send valid messages with valid credentials.
     const {FREEMOBILE_USERNAME: username, FREEMOBILE_PASSWORD: password} = process.env;
-    if (username && password) it('should send valid messages with valid credentials', async () => {
-      try {
-        await new Client(username, password).sendMessage('Bonjour Cédric !');
-        expect(true).to.be.ok;
-      }
+    if (username && password) try {
+      await new Client(username, password).sendMessage('Bonjour Cédric !');
+      expect(true).to.be.ok;
+    }
 
-      catch (err) {
-        expect(err).to.be.an.instanceof(ClientError);
-      }
-    });
-  });
-});
+    catch (err) {
+      expect(err).to.be.an.instanceof(ClientError);
+    }
+  }
+}
