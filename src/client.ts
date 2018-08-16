785a8f292ef19aa50d5b35f43e48bf9f60c42606
Cédric Belin <cedric@belin.io>
2018-08-16 22:22:56 +0200

    Changed the signature of the constructors

--- src/client.ts
+++ src/client.ts
@@ -19,20 +19,14 @@ export class Client extends EventEmitter {
   public static readonly eventResponse: string = 'response';
 
   /**
-   * The URL of the API end point.
-   */
-  public endPoint: URL;
-
-  /**
    * Creates a new client.
    * @param username The user name associated to the account.
    * @param password The identification key associated to the account.
    * @param endPoint The URL of the API end point.
    * @throws {TypeError} The account credentials are invalid.
    */
-  constructor(public username: string, public password: string, endPoint: string | URL = 'https://smsapi.free-mobile.fr') {
+  constructor(public username: string, public password: string, public readonly endPoint: URL = new URL('https://smsapi.free-mobile.fr')) {
     super();
-    this.endPoint = typeof endPoint == 'string' ? new URL(endPoint) : endPoint;
     if (!this.password.length || !this.username.length) throw new TypeError('The account credentials are invalid.');
   }
 
@@ -75,19 +69,13 @@ export class Client extends EventEmitter {
 export class ClientError extends Error {
 
   /**
-   * The URL of the HTTP request or response that failed.
-   */
-  public uri: URL | null;
-
-  /**
    * Creates a new client error.
    * @param message A message describing the error.
    * @param uri The URL of the HTTP request or response that failed.
    */
-  constructor(message: string = '', uri: null | string | URL = null) {
+  constructor(message: string = '', public readonly uri: URL | null = null) {
     super(message);
     this.name = 'ClientError';
-    this.uri = typeof uri == 'string' ? new URL(uri) : uri;
   }
 
   /**
