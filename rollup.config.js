2c3db062ccb62a0202f6cd3b1a8a3481b479362b
Cédric Belin <cedric@belin.io>
2019-01-21 22:45:34 +0100

    Reverted a previous commit

--- rollup.config.js
+++
@@ -1,7 +0,0 @@
-const commonjs = require('rollup-plugin-commonjs');
-
-module.exports = {
-  input: 'lib/index.js',
-  output: {file: 'build/free-mobile.js', format: 'iife', name: 'FreeMobile'},
-  plugins: [commonjs()]
-};
