7b88e430c5bc72ab13db0f981be3b789ce0c2817
Cédric Belin <cedric@belin.io>
2019-04-14 13:54:35 +0200

    Updated the build system

--- src/rollup.config.js
+++ src/rollup.config.js
@@ -1,9 +1,10 @@
+import {join} from 'path';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
-  input: 'lib/browser.mjs',
-  output: {file: 'build/free-mobile.js', format: 'iife', name: 'freeMobile'},
+  input: join(__dirname, '../lib/browser.mjs'),
+  output: {file: join(__dirname, '../build/free-mobile.js'), format: 'iife', name: 'freeMobile'},
   plugins: [resolve(), commonjs({
     namedExports: {'node_modules/eventemitter3/index.js': ['EventEmitter']}
   })]
