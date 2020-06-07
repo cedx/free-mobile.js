7f3b65c83b93fb7568b9a660207d25362100738d
Cédric Belin <cedric@belin.io>
2020-06-08 00:05:37 +0200

    Updated the tests

--- test/client_test.js
+++ test/client_test.js
@@ -5,23 +5,23 @@ import {Client, ClientError} from "../lib/index.js";
 describe("Client", function() {
 	this.timeout(15000);
 
-	describe("constructor", () => {
-		it("should throw an error if the credentials are invalid", () => {
+	describe("constructor", function() {
+		it("should throw an error if the credentials are invalid", function() {
 			assert.throws(() => new Client("", ""), TypeError);
 		});
 	});
 
-	describe(".sendMessage()", () => {
-		it("should not send invalid messages", () => {
+	describe(".sendMessage()", function() {
+		it("should not send invalid messages", function() {
 			assert.rejects(new Client("anonymous", "secret").sendMessage(""), TypeError);
 		});
 
-		it("should reject if a network error occurred", () => {
+		it("should reject if a network error occurred", function() {
 			const endPoint = new URL("http://localhost:10000/");
 			assert.rejects(new Client("anonymous", "secret", endPoint).sendMessage("Bonjour Cédric !"), ClientError);
 		});
 
-		it("should send valid messages with valid credentials", () => {
+		it("should send valid messages with valid credentials", function() {
 			const username = process.env.FREEMOBILE_USERNAME;
 			const password = process.env.FREEMOBILE_PASSWORD;
 			assert.doesNotReject(new Client(username, password).sendMessage("Bonjour Cédric, à partir de Node.js !"));
