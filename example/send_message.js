696b90ff9b3432b3c5ffa6207ddf9070c1951acf
Cédric Belin <cedric@belin.io>
2022-05-04 22:40:16 +0200

    Updated the example code

---
+++ example/send_message.js
@@ -0,0 +1,15 @@
+import console from "node:console";
+import {Client} from "@cedx/free-mobile";
+
+/**
+ * Sends an SMS notification.
+ */
+try {
+	const client = new Client("your account identifier", "your API key");
+	await client.sendMessage("Hello World from Node.js!");
+	console.log("The message was sent successfully.");
+}
+
+catch (error) {
+	console.log(`An error occurred: ${error}`);
+}
