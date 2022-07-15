826db5604cce457dbfe47c1f94494bc370cda25d
Cédric Belin <cedric@belin.io>
2022-07-15 13:29:10 +0200

    Using the new describe/it API

--- test/client_test.js
+++ test/client_test.js
@@ -1,20 +1,26 @@
 import assert from "node:assert/strict";
 import {env} from "node:process";
-import test from "node:test";
+// @ts-expect-error TS2614
+import {describe, it} from "node:test";
 import {Client} from "../src/index.js";
 
-test(".sendMessage()", async ctx => {
-	await ctx.test("should reject if a network error occurred", () => {
-		assert.rejects(new Client("anonymous", "secret", "http://localhost:10000/").sendMessage("Hello World!"));
-	});
+/**
+ * Tests the features of the {@link Client} class.
+ */
+describe("Client", () => {
+	describe(".sendMessage()", () => {
+		it("should reject if a network error occurred", () => {
+			assert.rejects(new Client("anonymous", "secret", "http://localhost:10000/").sendMessage("Hello World!"));
+		});
 
-	await ctx.test("should reject if the credentials are invalid", () => {
-		assert.rejects(new Client("anonymous", "secret").sendMessage("Hello World!"));
-	});
+		it("should reject if the credentials are invalid", () => {
+			assert.rejects(new Client("anonymous", "secret").sendMessage("Hello World!"));
+		});
 
-	await ctx.test("should send SMS messages if the credentials are valid", () => {
-		const account = env.FREEMOBILE_ACCOUNT ?? "";
-		const apiKey = env.FREEMOBILE_API_KEY ?? "";
-		return assert.doesNotReject(new Client(account, apiKey).sendMessage("Hello Cédric, from Node.js !"));
+		it("should send SMS messages if the credentials are valid", () => {
+			const account = env.FREEMOBILE_ACCOUNT ?? "";
+			const apiKey = env.FREEMOBILE_API_KEY ?? "";
+			return assert.doesNotReject(new Client(account, apiKey).sendMessage("Hello Cédric, from Node.js !"));
+		});
 	});
 });
