9e19a9b012b1e575e51035c3e4e3585f84090f6c
Cédric Belin <cedric@belin.io>
2024-01-22 21:51:09 +0100

    Update the tests

--- test/client_test.js
+++ test/client_test.js
@@ -1,4 +1,4 @@
-import {equal} from "node:assert/strict";
+import {doesNotReject, rejects} from "node:assert/strict";
 import {env} from "node:process";
 import {describe, it} from "node:test";
 import {Client} from "#free_mobile";
@@ -9,17 +9,17 @@ import {Client} from "#free_mobile";
 describe("Client", () => {
 	describe("sendMessage()", () => {
 		it("should reject if a network error occurred", () => {
-			assert.rejects(new Client("anonymous", "secret", "http://localhost:10000/").sendMessage("Hello World!"));
+			rejects(new Client("anonymous", "secret", "http://localhost:10000/").sendMessage("Hello World!"));
 		});
 
 		it("should reject if the credentials are invalid", () => {
-			assert.rejects(new Client("anonymous", "secret").sendMessage("Hello World!"));
+			rejects(new Client("anonymous", "secret").sendMessage("Hello World!"));
 		});
 
 		it("should send SMS messages if the credentials are valid", () => {
 			const account = env.FREEMOBILE_ACCOUNT ?? "";
 			const apiKey = env.FREEMOBILE_API_KEY ?? "";
-			return assert.doesNotReject(new Client(account, apiKey).sendMessage("Hello Cédric, from Node.js !"));
+			return doesNotReject(new Client(account, apiKey).sendMessage("Hello Cédric, from Node.js !"));
 		});
 	});
 });
