3ab0899ae98e38bd2619cddc1eb217198021174b
Cédric Belin <cedx@outlook.com>
2025-11-02 13:37:56 +0100

    Remove the `account` and `apiKey` properties from the `Client` class

--- src/Client.ts
+++ src/Client.ts
@@ -4,31 +4,31 @@
 export class Client {
 
 	/**
-	 * The Free Mobile account.
-	 */
-	readonly account: string;
-
-	/**
-	 * The Free Mobile API key.
-	 */
-	readonly apiKey: string;
-
-	/**
 	 * The base URL of the remote API endpoint.
 	 */
 	readonly baseUrl: URL;
 
 	/**
+	 * The Free Mobile password.
+	 */
+	readonly #password: string;
+
+	/**
+	 * The Free Mobile username.
+	 */
+	readonly #username: string;
+
+	/**
 	 * Creates a new client.
-	 * @param account The Free Mobile account.
-	 * @param apiKey The Free Mobile API key.
+	 * @param username The Free Mobile username.
+	 * @param password The Free Mobile password.
 	 * @param baseUrl The base URL of the remote API endpoint.
 	 */
-	constructor(account: string, apiKey: string, baseUrl: string|URL = "https://smsapi.free-mobile.fr") {
+	constructor(username: string, password: string, baseUrl: string|URL = "https://smsapi.free-mobile.fr") {
 		const url = baseUrl instanceof URL ? baseUrl.href : baseUrl;
-		this.account = account;
-		this.apiKey = apiKey;
 		this.baseUrl = new URL(url.endsWith("/") ? url : `${url}/`);
+		this.#username = username;
+		this.#password = password;
 	}
 
 	/**
@@ -37,7 +37,7 @@ export class Client {
 	 * @returns Resolves when the message has been sent.
 	 */
 	async sendMessage(message: string): Promise<void> {
-		const query = new URLSearchParams({msg: message.trim().slice(0, 160), pass: this.apiKey, user: this.account});
+		const query = new URLSearchParams({msg: message.trim().slice(0, 160), pass: this.#password, user: this.#username});
 		const response = await fetch(new URL(`sendmsg?${query}`, this.baseUrl), {headers: {"User-Agent": navigator.userAgent}});
 		if (!response.ok) throw new Error(`${response.status} ${response.statusText}`);
 	}
