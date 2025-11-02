6c473ddd89cc1beed19720c11d2157aa6985b581
Cédric Belin <cedx@outlook.com>
2025-11-02 14:51:38 +0100

    Code formatting

--- src/Client.ts
+++ src/Client.ts
@@ -14,20 +14,20 @@ export class Client {
 	readonly #password: string;
 
 	/**
-	 * The Free Mobile username.
+	 * The Free Mobile user name.
 	 */
-	readonly #username: string;
+	readonly #userName: string;
 
 	/**
 	 * Creates a new client.
-	 * @param username The Free Mobile username.
+	 * @param userName The Free Mobile user name.
 	 * @param password The Free Mobile password.
 	 * @param baseUrl The base URL of the remote API endpoint.
 	 */
-	constructor(username: string, password: string, baseUrl: string|URL = "https://smsapi.free-mobile.fr") {
+	constructor(userName: string, password: string, baseUrl: string|URL = "https://smsapi.free-mobile.fr") {
 		const url = baseUrl instanceof URL ? baseUrl.href : baseUrl;
 		this.baseUrl = new URL(url.endsWith("/") ? url : `${url}/`);
-		this.#username = username;
+		this.#userName = userName;
 		this.#password = password;
 	}
 
@@ -37,7 +37,7 @@ export class Client {
 	 * @returns Resolves when the message has been sent.
 	 */
 	async sendMessage(message: string): Promise<void> {
-		const query = new URLSearchParams({msg: message.trim().slice(0, 160), pass: this.#password, user: this.#username});
+		const query = new URLSearchParams({msg: message.trim().slice(0, 160), pass: this.#password, user: this.#userName});
 		const response = await fetch(new URL(`sendmsg?${query}`, this.baseUrl), {headers: {"User-Agent": navigator.userAgent}});
 		if (!response.ok) throw new Error(`${response.status} ${response.statusText}`);
 	}
